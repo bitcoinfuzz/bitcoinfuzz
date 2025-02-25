c40d6e0318e3e82957dd168e8a6d292d31ff1bb0
Bruno Garcia <brunoely.gc@gmail.com>
2025-02-25 16:03:16 -0300

    driver: add MiniscriptParseTarget

--- driver.cpp
+++ driver.cpp
@@ -40,7 +40,7 @@ namespace bitcoinfuzz
         }
     }
 
-    void Driver::ScriptEvalTarget(std::span<const uint8_t> buffer) const 
+    void Driver::ScriptEvalTarget(std::span<const uint8_t> buffer) const
     {
         FuzzedDataProvider provider(buffer.data(), buffer.size());
         std::vector<uint8_t> input_data = provider.ConsumeBytes<uint8_t>(
@@ -59,7 +59,7 @@ namespace bitcoinfuzz
         }
     }
 
-    void Driver::DescriptorParseTarget(std::span<const uint8_t> buffer) const 
+    void Driver::DescriptorParseTarget(std::span<const uint8_t> buffer) const
     {
         FuzzedDataProvider provider(buffer.data(), buffer.size());
         std::string desc{provider.ConsumeRemainingBytesAsString()};
@@ -73,6 +73,23 @@ namespace bitcoinfuzz
         }
     }
 
+    void Driver::MiniscriptParseTarget(std::span<const uint8_t> buffer) const
+    {
+        FuzzedDataProvider provider(buffer.data(), buffer.size());
+        std::string miniscript{provider.ConsumeRemainingBytesAsString()};
+        // Skip these cases
+        if (miniscript == "1" || miniscript == "0") return;
+        std::optional<bool> last_response{std::nullopt};
+        for (auto& module : modules)
+        {
+            std::optional<bool> res{module.second->miniscript_parse(miniscript)};
+            if (!res.has_value()) continue;
+            if (last_response.has_value()) assert(*res == *last_response);
+            last_response = *res;
+        }
+    }
+
+
     void Driver::Run(const uint8_t *data, const size_t size, const std::string &target) const
     {
         std::span<const uint8_t> buffer{data, size};
@@ -84,7 +101,9 @@ namespace bitcoinfuzz
             this->ScriptEvalTarget(buffer);
         } else if (target == "descriptor_parse") {
             this->DescriptorParseTarget(buffer);
-        } else {
+        } else if (target == "miniscript_parse") {
+	        this->MiniscriptParseTarget(buffer);
+	} else {
             std::cout << "Target not defined!" << std::endl;
             assert(false);
         }
