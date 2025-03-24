4eb64beb0d01fe1a2582e8376bc6d3a6d264305b
Erick Cestari <erickcestari03@gmail.com>
2025-03-24 11:56:38 -0300

    fix: embit miniscript parse by using from_string instead read_from

--- modules/embit/embit_lib/embit_lib.py
+++ modules/embit/embit_lib/embit_lib.py
@@ -1,15 +1,14 @@
-
 from io import BytesIO
 from embit.descriptor.miniscript import Miniscript
 
 def miniscript_parse(input):
     try:
-        ms = Miniscript.read_from(BytesIO(input.encode()), taproot=False)
+        ms = Miniscript.from_string(input, taproot=False)
         ms.verify()
         return True
     except Exception as _:
         try:
-            ms = Miniscript.read_from(BytesIO(input.encode()), taproot=True)
+            ms = Miniscript.from_string(input, taproot=True)
             ms.verify()
             return True
         except Exception as _:
