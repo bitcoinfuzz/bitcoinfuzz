27122c90c1a4c6c8c51e44b7a9e2b0150bc1d698
Dikshant <dikshant.073@gmail.com>
2025-04-18 17:04:17 +0530

    removed hardcoded fallbacks from lnd
    Signed-off-by: Dikshant <dikshant.073@gmail.com>

--- modules/lnd/lnd_wrapper/wrapper.go
+++ modules/lnd/lnd_wrapper/wrapper.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/lightningnetwork/lnd/zpay32"
@@ -82,168 +83,183 @@ func LndConstructHtlcSuccessTx(commitmentTxHex *C.char, htlcIndex C.uint, preima
 
 	runtime.GC()
 
+	// if inputs = nil, create a minimal valid tx
 	if commitmentTxHex == nil || preimage == nil {
-		// return fallback tx
-		var sb strings.Builder
-		sb.WriteString("020000000100000000000000000000000000000000000000000000000000000000000000000000000000ffffffff0140420f0000000000160014000000000000000000000000000000000000000000000000")
-		return C.CString(sb.String())
+		//  minimal valid tx with zero inputs/outputs
+		minTx := wire.NewMsgTx(2)
+
+		// dummy input with the htlcIndex as vout
+		dummyHash := chainhash.Hash{}
+		outpoint := wire.NewOutPoint(&dummyHash, uint32(htlcIndex))
+		txIn := wire.NewTxIn(outpoint, nil, nil)
+		minTx.AddTxIn(txIn)
+
+		// dummy output
+		pkScript, _ := txscript.NewScriptBuilder().
+			AddOp(txscript.OP_0).
+			AddData(make([]byte, 20)).
+			Script()
+
+		txOut := wire.NewTxOut(1000000, pkScript)
+		minTx.AddTxOut(txOut)
+
+		var buf bytes.Buffer
+		minTx.Serialize(&buf)
+		return C.CString(hex.EncodeToString(buf.Bytes()))
 	}
 
 	commitmentTxHexStr := C.GoString(commitmentTxHex)
 	preimageStr := C.GoString(preimage)
 
-	// parse the commitment tx
-	commitmentTxBytes, err := hex.DecodeString(commitmentTxHexStr)
-	if err != nil {
-		// return fallback tx
-		var sb strings.Builder
-		sb.WriteString("020000000100000000000000000000000000000000000000000000000000000000000000000000000000ffffffff0140420f0000000000160014000000000000000000000000000000000000000000000000")
-		return C.CString(sb.String())
-	}
-
+	// parse commitment tx (if it fails use dummy tx but preserve htlcIndex)
 	var commitmentTx wire.MsgTx
-	if err := commitmentTx.Deserialize(bytes.NewReader(commitmentTxBytes)); err != nil {
-		// return fallback tx
-		var sb strings.Builder
-		sb.WriteString("020000000100000000000000000000000000000000000000000000000000000000000000000000000000ffffffff0140420f0000000000160014000000000000000000000000000000000000000000000000")
-		return C.CString(sb.String())
+	commitmentTxBytes, err := hex.DecodeString(commitmentTxHexStr)
+	if err != nil || commitmentTx.Deserialize(bytes.NewReader(commitmentTxBytes)) != nil {
+		// dummy commitment tx with one output
+		commitmentTx = *wire.NewMsgTx(2)
+		dummyScript, _ := txscript.NewScriptBuilder().
+			AddOp(txscript.OP_RETURN).
+			Script()
+		commitmentTx.AddTxOut(wire.NewTxOut(1000000, dummyScript))
 	}
 
-	// parse preimage
+	// parse preimage (if invalid use dummy preimage)
 	preimageBytes, err := hex.DecodeString(preimageStr)
 	if err != nil || len(preimageBytes) != 32 {
 		preimageBytes = make([]byte, 32)
 	}
 
-	// find HTLC output in commitment tx
-	if int(htlcIndex) >= len(commitmentTx.TxOut) {
-		// return fallback tx
-		var sb strings.Builder
-		sb.WriteString("020000000100000000000000000000000000000000000000000000000000000000000000000000000000ffffffff0140420f0000000000160014000000000000000000000000000000000000000000000000")
-		return C.CString(sb.String())
+	// htlcIndex (if invalid use index 0 but preserve original index in outpoint)
+	txOutIndex := uint32(0)
+	if int(htlcIndex) < len(commitmentTx.TxOut) {
+		txOutIndex = uint32(htlcIndex)
 	}
 
-	// construct HTLC success tx (simplified version)
+	// HTLC success tx
 	successTx := wire.NewMsgTx(2)
 
-	// add input spending the HTLC output
+	// add input spending to the HTLC output (use the original htlcIndex even if invalid)
 	txHash := commitmentTx.TxHash()
 	outpoint := wire.NewOutPoint(&txHash, uint32(htlcIndex))
 	txIn := wire.NewTxIn(outpoint, nil, nil)
 	successTx.AddTxIn(txIn)
 
-	// calculate output amount after fees (dummy output)
-	inputAmount := commitmentTx.TxOut[htlcIndex].Value
+	// output amount
+	var inputAmount int64 = 1000000 // Default if index is invalid
+	if int(txOutIndex) < len(commitmentTx.TxOut) {
+		inputAmount = commitmentTx.TxOut[txOutIndex].Value
+	}
+
 	feeAmount := int64(feeRate * 250 / 1000)
 	outputAmount := inputAmount - feeAmount
 	if outputAmount < 0 {
 		outputAmount = 0
 	}
 
-	// dummy P2WPKH output script
+	// output script
 	pkScript, _ := txscript.NewScriptBuilder().
 		AddOp(txscript.OP_0).
-		AddData(make([]byte, 20)). // dummy pubkey hash
+		AddData(make([]byte, 20)).
 		Script()
 
 	txOut := wire.NewTxOut(outputAmount, pkScript)
 	successTx.AddTxOut(txOut)
 
 	var buf bytes.Buffer
-	if err := successTx.Serialize(&buf); err != nil {
-		// return fallback tx
-		var sb strings.Builder
-		sb.WriteString("020000000100000000000000000000000000000000000000000000000000000000000000000000000000ffffffff0140420f0000000000160014000000000000000000000000000000000000000000000000")
-		return C.CString(sb.String())
-	}
-
-	var sb strings.Builder
-	sb.WriteString(hex.EncodeToString(buf.Bytes()))
-
-	return C.CString(sb.String())
+	successTx.Serialize(&buf)
+	return C.CString(hex.EncodeToString(buf.Bytes()))
 }
 
 //export LndConstructHtlcTimeoutTx
 func LndConstructHtlcTimeoutTx(commitmentTxHex *C.char, htlcIndex C.uint, cltvExpiry C.uint, feeRate C.ulonglong) *C.char {
-
 	runtime.GC()
 
+	// if input = nil (create a minimal valid tx)
 	if commitmentTxHex == nil {
-		// return fallback tx
-		var sb strings.Builder
-		sb.WriteString("020000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000140420f0000000000160014000000000000000000000000000000000000000000000000")
-		return C.CString(sb.String())
+		// minimal valid tx with zero inputs/outputs
+		minTx := wire.NewMsgTx(2)
+
+		// add dummy input with htlcIndex as vout
+		dummyHash := chainhash.Hash{}
+		outpoint := wire.NewOutPoint(&dummyHash, uint32(htlcIndex))
+		txIn := wire.NewTxIn(outpoint, nil, nil)
+		txIn.Sequence = 0 // enable locktime
+		minTx.AddTxIn(txIn)
+
+		// set locktime
+		minTx.LockTime = uint32(cltvExpiry)
+
+		// add dummy output
+		pkScript, _ := txscript.NewScriptBuilder().
+			AddOp(txscript.OP_0).
+			AddData(make([]byte, 20)).
+			Script()
+
+		txOut := wire.NewTxOut(1000000, pkScript)
+		minTx.AddTxOut(txOut)
+
+		var buf bytes.Buffer
+		minTx.Serialize(&buf)
+		return C.CString(hex.EncodeToString(buf.Bytes()))
 	}
 
 	commitmentTxHexStr := C.GoString(commitmentTxHex)
 
-	// parse commitment tx
-	commitmentTxBytes, err := hex.DecodeString(commitmentTxHexStr)
-	if err != nil {
-		// return fallback tx
-		var sb strings.Builder
-		sb.WriteString("020000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000140420f0000000000160014000000000000000000000000000000000000000000000000")
-		return C.CString(sb.String())
-	}
-
+	// parse commitment tx (if it fails use dummy tx but preserve htlcIndex)
 	var commitmentTx wire.MsgTx
-	if err := commitmentTx.Deserialize(bytes.NewReader(commitmentTxBytes)); err != nil {
-		// return fallback tx
-		var sb strings.Builder
-		sb.WriteString("020000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000140420f0000000000160014000000000000000000000000000000000000000000000000")
-		return C.CString(sb.String())
+	commitmentTxBytes, err := hex.DecodeString(commitmentTxHexStr)
+	if err != nil || commitmentTx.Deserialize(bytes.NewReader(commitmentTxBytes)) != nil {
+		// dummy commitment tx with one output
+		commitmentTx = *wire.NewMsgTx(2)
+		dummyScript, _ := txscript.NewScriptBuilder().
+			AddOp(txscript.OP_RETURN).
+			Script()
+		commitmentTx.AddTxOut(wire.NewTxOut(1000000, dummyScript))
 	}
 
-	// find HTLC output in commitment tx
-	if int(htlcIndex) >= len(commitmentTx.TxOut) {
-		// return fallback tx
-		var sb strings.Builder
-		sb.WriteString("020000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000140420f0000000000160014000000000000000000000000000000000000000000000000")
-		return C.CString(sb.String())
+	// check htlcIndex (if invalid use index 0 but preserve original index in outpoint)
+	txOutIndex := uint32(0)
+	if int(htlcIndex) < len(commitmentTx.TxOut) {
+		txOutIndex = uint32(htlcIndex)
 	}
 
-	// HTLC timeout tx (simplified)
+	// HTLC timeout tx
 	timeoutTx := wire.NewMsgTx(2)
 
-	// input spending the HTLC output
+	// add input spending the HTLC output (use original htlcIndex even if invalid)
 	txHash := commitmentTx.TxHash()
 	outpoint := wire.NewOutPoint(&txHash, uint32(htlcIndex))
 	txIn := wire.NewTxIn(outpoint, nil, nil)
 	txIn.Sequence = 0 // enable locktime
 	timeoutTx.AddTxIn(txIn)
 
-	// set locktime to CLTV expiry
+	// set locktime
 	timeoutTx.LockTime = uint32(cltvExpiry)
 
-	// output amount after fees (dummy output)
-	inputAmount := commitmentTx.TxOut[htlcIndex].Value
+	// output amount
+	var inputAmount int64 = 1000000 // Default if index is invalid
+	if int(txOutIndex) < len(commitmentTx.TxOut) {
+		inputAmount = commitmentTx.TxOut[txOutIndex].Value
+	}
+
 	feeAmount := int64(feeRate * 250 / 1000)
 	outputAmount := inputAmount - feeAmount
 	if outputAmount < 0 {
 		outputAmount = 0
 	}
 
-	// P2WPKH output script (dummy)
+	// create output script
 	pkScript, _ := txscript.NewScriptBuilder().
 		AddOp(txscript.OP_0).
-		AddData(make([]byte, 20)). // Dummy pubkey hash
+		AddData(make([]byte, 20)).
 		Script()
 
 	txOut := wire.NewTxOut(outputAmount, pkScript)
 	timeoutTx.AddTxOut(txOut)
 
 	var buf bytes.Buffer
-	if err := timeoutTx.Serialize(&buf); err != nil {
-		// return fallback tx
-		var sb strings.Builder
-		sb.WriteString("020000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000140420f0000000000160014000000000000000000000000000000000000000000000000")
-		return C.CString(sb.String())
-	}
-
-	var sb strings.Builder
-	sb.WriteString(hex.EncodeToString(buf.Bytes()))
-
-	return C.CString(sb.String())
+	timeoutTx.Serialize(&buf)
+	return C.CString(hex.EncodeToString(buf.Bytes()))
 }
 
 func main() {}
