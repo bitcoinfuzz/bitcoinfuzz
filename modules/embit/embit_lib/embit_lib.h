e8fc691b4c293628c4e0eea5d8bb1f78720ef163
Erick Cestari <erickcestari03@gmail.com>
2025-05-28 15:11:17 -0300

    refactor(embit): remove additional embit_lib.cpp file

--- modules/embit/embit_lib/embit_lib.h
+++
@@ -1,8 +0,0 @@
-#include <cstddef>
-#include <cstdint>
-
-extern "C" {
-    bool embit_miniscript_parse(const char* input);
-    bool embit_descriptor_parse(const char* input);
-    char* embit_psbt_parse(const uint8_t* data, size_t len);
-}
